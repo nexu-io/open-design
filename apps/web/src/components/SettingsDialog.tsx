75a2fb973c0cecdfceba5d0a0f3c6078e9e46b28
Ziqi Xu <ziqi@MacBook-Pro.local>
2026-07-07 11:29:14 +0200

    Polish agent inventory dialog layout

--- apps/web/src/components/SettingsDialog.tsx
+++ apps/web/src/components/SettingsDialog.tsx
@@ -7837,7 +7837,7 @@ function InventoryDetailDialog({
             aria-label={t('common.close')}
             title={t('common.close')}
           >
-            <Icon name="close" size={16} />
+            <Icon name="close" size={18} />
           </button>
         </header>
         <label className="agent-inventory-search">
