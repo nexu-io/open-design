55417c167135723021f21056c00f4c57f9338034
a1chzt <chizblank@gmail.com>
2026-09-26 15:42:31 +0800

    chore(web): restore generated next-env.d.ts accidentally included in 30361fd670

--- apps/web/next-env.d.ts
+++ apps/web/next-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-import "./../../.tmp/e2e/playwright-30175-9dd75b19-w0/i0-p30183/scratch/tools-dev/playwright-30175-9dd75b19-w0/web/next/dev/types/routes.d.ts";
+import "./.next/types/routes.d.ts";
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
